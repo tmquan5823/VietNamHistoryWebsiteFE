2d7dcead94f61b74c5a58c4926f2121c6e2414c8
tmquan5823 <tmquan0948628477@gmail.com>
2025-04-19 23:50:08 +0700

    initial commit

---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router";
+import Layout from "./components/Layout";
+import { ROUTERS } from "./constant";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path={ROUTERS.DEFAULT} element={<Home />} />
+        <Route path={ROUTERS.HOME} element={<Home />} />
+        <Route path={ROUTERS.LOGIN} element={<Login />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
